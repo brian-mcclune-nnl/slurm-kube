d3632eed9b00bf48b49dd70d43970511ab166d0d
Brian McClune <brian.mcclune@unnpp.gov>
2025-05-09 19:49:23 +0000

    Add cli_filter/lua support
    Implement setup_defaults and post_submit required functions

--- slurm-kube/cli_filter.lua
+++ slurm-kube/cli_filter.lua
@@ -1,3 +1,14 @@
 function slurm_cli_pre_submit(options, pack_offset)
-  slurm.log_info("Hello, world!")
+  slurm.log_info("in pre_submit")
+  return slurm.SUCCESS
+end
+
+function slurm_cli_setup_defaults(options, early_pass)
+  slurm.log_info("in setup_defaults (early_pass: %s)", tostring(early_pass))
+  return slurm.SUCCESS
+end
+
+function slurm_cli_post_submit(offset, job_id, step_id)
+  slurm.log_info("in post_submit")
+  return slurm.SUCCESS
 end
